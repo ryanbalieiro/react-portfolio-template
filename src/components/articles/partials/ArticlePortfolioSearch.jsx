b1a525284a730d474456cf9680443560552febb8
sonvothai-zigexn-ventura <sonvothai@zigexn.vn>
2026-05-31 23:38:40 +0700

    feat: add ArticlePortfolioSearch component

---
+++ src/components/articles/partials/ArticlePortfolioSearch.jsx
@@ -0,0 +1,28 @@
+import "./ArticlePortfolioSearch.scss"
+import React from 'react'
+
+function ArticlePortfolioSearch({ query, setQuery, placeholder = "Search..." }) {
+    return (
+        <div className={`article-portfolio-search`}>
+            <i className={`article-portfolio-search-icon fa-solid fa-magnifying-glass`}/>
+            <input
+                className={`article-portfolio-search-input text-3`}
+                type="text"
+                value={query}
+                placeholder={placeholder}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            {query && (
+                <button
+                    className={`article-portfolio-search-clear`}
+                    type="button"
+                    onMouseDown={() => setQuery("")}
+                    onTouchStart={() => setQuery("")}>
+                    <i className={`fa-solid fa-xmark`}/>
+                </button>
+            )}
+        </div>
+    )
+}
+
+export default ArticlePortfolioSearch
